fdcb44743897363659250e776e965025a19f0844
Tama1i <ryazantseff.matvei@yandex.ru>
2022-11-04 13:17:22 +0300

    fix all 1

--- idz/3.py
+++ idz/3.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 if __name__ == '__main__':
     for i in range(1, 10):
-       for j in range(1, 10):
-           print(i * j, end = "\t")
-       print("\n")
+        for j in range(1, 10):
+            print(i * j, end = "\t")
+        print("\n")
--- pov.py
+++ pov.py
@@ -1,11 +1,11 @@
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 import math
 import sys
 
-# Постоянная Эйлера.
 EULER = 0.5772156649015328606
-# Точность вычислений.
 EPS = 1e-10
 
 if __name__ == '__main__':
@@ -13,16 +13,11 @@ if __name__ == '__main__':
     if x == 0:
         print("Illegal value of x", file=sys.stderr)
         exit(1)
-
     a = x
-    S, n = a , 1
-
-    # Найти сумму членов ряда.
+    S, n = a, 1
     while math.fabs(a) > EPS:
-        a *= (-1) * (((x ** 2) * ((2 * n) + 1) / (((2 * n + 3) ** 2) * ((2 * n) + 2)))) 
+        a *= -(((x**2) * ((2 * n) + 1)) / ((((2 * n) + 3)**2) * ((2 * n)) + 2))
         S += a
         n += 1
 
-    # Вывести значение функции.
-    print(f"Si({x}) = {EULER + math.log(math.fabs(x)) + S}")
-
+print(f"Ei({x}) = {EULER + math.log(math.fabs(x)) + S}")
